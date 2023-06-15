e6900e2d54c1cc7929e59cbefb941c041b38bae3
Moksh Agrawal <111683521+Moksh-10@users.noreply.github.com>
2023-06-15 16:19:36 +0530

    Add files via upload

---
+++ lc744.java
@@ -0,0 +1,30 @@
+public class lc744 {
+    public static void main(String[] args) {
+        
+    }
+
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+            
+            
+            int start=0;
+            int end=letters.length-1;
+    
+        //boolean isAsc = arr[start]<arr[end];
+    
+        while(start<=end){
+            int mid=start+(end-start)/2;
+    
+           if(target<letters[mid]){
+            end=mid-1;
+           } else {
+            start=mid+1;
+           }
+        }
+        return letters[start%letters.length];
+    
+    }
+
+    
+
+}
